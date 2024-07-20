f8c895ab3b0d9986d10edd29b771bb526ce067c5
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-07-20 16:49:52 +0530

    added styles

--- google-keep/src/app/ui/Navbar.tsx
+++ google-keep/src/app/ui/Navbar.tsx
@@ -1,7 +1,8 @@
+import styles from '@/app/ui/styles/test.module.css'
 const Navbar = () => {
     return(
         <div>
-            <h1>This is navbar should be on top</h1>
+            <h1 className={styles.color}>This is navbar should be on top</h1>
             </div>
     )
 }
