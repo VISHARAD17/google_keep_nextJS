ad8d08e6a203bb491c6818ab55842a374814702d
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-07-29 22:29:48 +0530

    remove unused imports

--- google-keep/src/app/api/graphql/route.ts
+++ google-keep/src/app/api/graphql/route.ts
@@ -1,21 +1,8 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
-import { gql } from 'graphql-tag';
 
 import { typeDefs } from '../typeDef';
 import { resolvers } from '../resolver';
-
-// const resolvers = {
-//   Query: {
-//     hello: () => 'world',
-//   },
-// };
-//
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
 
 const server = new ApolloServer({
   resolvers,
