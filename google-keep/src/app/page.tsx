fc4a94a355cbc88340ccedc71606c5db61d360c8
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-07-30 14:57:03 +0530

    attempt #1 authentication using next-js auth

--- google-keep/src/app/page.tsx
+++ google-keep/src/app/page.tsx
@@ -1,4 +1,3 @@
-import { BookList } from "./ui/components/BookList/BookList";
 const Home = () => {
     const Notes = [
         { title:"title 1", content:"content something lorem ipsum" },
@@ -7,23 +6,7 @@ const Home = () => {
     ]
     return(
         <div>
-            <h1>This is Home Page</h1>     
-            <div>below this all static list notes</div>
-                <div>
-                        {Notes.map((note, index) => (
-                        <div key={index}>
-                            <ul>
-                                <li>title:{note.title}</li>
-                                <li>title:{note.content}</li>
-                            </ul>
-                            </div>
-                        ))}
-                </div>
-                <br/>
-                <div>
-                    <p> List of all books from DB</p>
-                    <BookList/>
-                </div>
+            <p>User sign in informations</p>
         </div>
     )
 }
